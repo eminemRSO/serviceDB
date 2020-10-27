5756a8fe82019e0e79e0366c6539d18b58426b0c
mihastravs <miha.stravs96@gmail.com>
2020-10-27 19:16:13 +0100

    used example and renamed the files as a starting point

---
+++ servicesDB/api/src/main/java/si/fri/rso/servicesDB/api/v1/ImageMetadataApplication.java
@@ -0,0 +1,9 @@
+package si.fri.rso.servicesDB.api.v1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/v1")
+public class ImageMetadataApplication extends Application {
+
+}
